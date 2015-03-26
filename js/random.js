063a7437c78f4dd7e7e4787f344756663496c762
Bryan Santos <201210086@gordoncollegeccs.edu.ph>
2015-03-26 01:00:32 -0700

    LATEST

---
+++ js/random.js
@@ -0,0 +1,121 @@
+function randomTagalogLoveQuotes()
+			{
+			var foodstalls=["Minsan sumigaw ako ng ayaw ko ng magmahal,Pero nung nakita kita napasigaw ulit ako ng joke lang yun.",
+							"Kung mahal mo ang isang tao. Sabihin mo sa kanya araw-araw. Hindi yung sa umpisa lang.",
+							"Hindi ko man maisigaw sa buong mundo kung sino ang mahal ko,sapat na siguro na alam nating pareho na ikaw ang tinutukoy ko.",
+							"Sa lahat ng emosyon na ipinakita ng tao. Selos ang pinakamahirap itago.",
+							"Wag ka maghanap ng taong makakaintindi sayo. Ang hanapin mo yung taong kahit hindi ka naiintindihan. hindi ka pa rin iniiwan.",
+							"Kahit ilang beses pa akong masaktan dahil sayo, di kita iiwan, di ako susuko. Dahil kung may 100 dahilan para iwan ka, hahanap pa rin ako ng isang dahilanpara ipaglaban ka.",
+							"Sana kaya kong ibigay ang buong mundo sayo para malaman mo kung gaano ka kahalaga sa buhay ko.",
+							"Ikaw ang pinakamagandang bagay na dumating sa akin, kaya hindi ko hahayaan na basta ka na lang mawawala sakin.",
+							"minsan hindi mo na kailangan tanungin sa kanya kung mahal ka niya kasi sa paraan pa lang nang pag trato niya malalaman mo na",
+							"ang taong hindi nakakasawang sabihin ng mahal kita ay yung taong marunong magparamdam na mas mahal ka niya.",
+							"Hindi mo na kailangan pang sabihin na mahal mo ako. Minsan isang matamis na ngiti lang galing sa magaganda mong mga labi ay sapat na para makumpleto ang buhay ko.",
+							"Hindi mo na kailangan pang sabihin na mahal mo ako. Minsan isang matamis na ngiti lang galing sa magaganda mong mga labi ay sapat na para makumpleto ang buhay ko.",
+							"Hindi mo na kailangan pang sabihin na mahal mo ako. Minsan isang matamis na ngiti lang galing sa magaganda mong mga labi ay sapat na para makumpleto ang buhay ko."];
+			var randNum= Math.floor(Math.random()*13);
+			document.getElementById('txtEat').innerHTML = foodstalls[randNum];
+			}
+			
+
+function randomLove()
+			{
+			var Love=["Ang love parang gobyerno, marami kang pag pipilian pero di mo alam kung alin ang totoo.",
+					"Ang love parang teacher, Tuturuan ka para di na maulit ang ngyare sayo, at masasaktan ka para malaman mo ang pagkakamali mo.",
+					"Ang love parang pag papatago ng pera, dpat pumili ka muna kung sinu yung mapagkakatiwalaan hindi ung oo nlng ng oo tulad nung isa dyan.",
+					"Ang love parang krimen, pwede mong itanggi, pwede mo ring aminin at hindi ka makakasiguro kung wala kang ebidensya.",
+					"Ang love parang pag sshopping, hirap piliin kung alin ang nararapat piliin.",
+					"Ang love parang bubble gum, dahil kht anong nguya ang gwin mo, mawawala at mwawala parin ang tamis nito.",
+					"Ang love parang gambling, no certainity of winning",
+					"Ang love parang laro lng yan, kaya mo bang ipanalo ang game kung reserba ka lang?",
+					"Ang love parang rosary, puno ng misteryo.",
+					"Ang love parang alak, habang tumatagal lalong sumasarap.",
+					"Ang love parang sakit ng tiyan, dahil pag nakipagaway ka dyan, di mo kaya ang sakit na binibigay nyan.",
+					"Ang love parang pag-iiskateboard, di mo maiintindihan hanggang di ka nasasaktan.",
+					"Ang love parang computer virus, sinisira lahat ng pinaghirapan.",
+					"Ang love parang pera, kapag fake, walang halaga. :)",
+					"Ang Love parang IKAW, PAASA.",
+					"Ang love parang Atm, hindi pwedeng kuha lng ng kuha, dapat paminsan minsan nag dedeposit ka.",
+					"Ang love parang cellphone, sa una mahal na mahal pero pag tumagal, wala nang pakielam kahit mabagsak.ng love parang football. Matapos mong sipain, saka mo lang hahabulin.",
+					"Ang LOVE parang Fruit Salad, matamis, makulay at madaming sahog.",
+					"Ang Love parang BUWAN, iiwan ka din pagdating ng ARAW",
+					"Ang LOVE parang ulan. bubuhos bigla tapos mawawala din bigla.",
+					"Ang pag-ibig parang sipon, why do you keep pulling it back when it’s better to let go?"
+					];
+			var randNum= Math.floor(Math.random()*3);
+			document.getElementById('txtLove').innerHTML = Love[randNum];
+			}
+			
+function randomHugot()
+			{
+			var Hugot=["Ang hirap tumawa kung di ka naman talaga masaya, Kahit ngumiti ka pahalata parin sa mga mata ang lungkot na nadaram",
+					"Minsan kahit ano pang i-advice mo sa isang tao. Di parin nila kayang gawin kasi takot na takot sila magkamali at mawala yung taong mahalaga sa kanila.",
+					"Kung mahal mo, habulin mo, ipaglaban mo. ‘Wag mong hintaying may magtulak sa kanya pabalik sa’yo. Hilahin mo. Hanggang kaya mo, wag kang bibitaw. Sorry, mahal ko eh.",
+					"Ang pag momove-on parang pag gising sa umaga, mahirap pero kelangan",
+					"Parang MRT lang yan, wag ka na mag-isip kung maghihintay ka ba, kung sasakay ka ba, at kung kakasya ka pa... Mag-taxi ka na lang.",
+					"Parang MRT lang yan, wag ka na mag-isip kung maghihintay ka ba, kung sasakay ka ba, at kung kakasya ka pa... Mag-taxi ka na lang.",
+					"Kapag mas masaya ka na mag-isa, alam mong nasa maling relasyon ka.",
+					"Gusto mong magreklamo, magdemand, na sana ikaw na lang, ikaw na lang talaga, pero alaam mong wala ka sa lugar.",
+					"Hanapin mo yung taong gagawin kang priority , hindi yung gagawin kang temporary.	",
+					"Hindi ako pera na kailangang itago at paunlarin sa bangko. Isa akong trophy na dapat ay naka-display, pinagmamalaki.",
+					"Marami pa ring lalaking nagsasabi ng totoo, sadyang hindi mo lang pinapaniwalaan sa ngayon kasi nasaktan ka na noon.",
+					"Kaya masakit at mahirap kasi matagal mo nang alam, ayaw mo lang tanggapin ang katotohanan.",
+					"dimo na maibabalik ang dati. nakaraan na e wala e walang permanente sa mundo kundi pagbabago"];
+			var randNum= Math.floor(Math.random()*13);
+			document.getElementById('txtHugot').innerHTML = Hugot[randNum];
+			}
+			
+
+			function randomEnglishSadLoveQuotes()
+			{
+			var SLQ=["Tears are words the heart can't say.",
+					"Don't cry because it's over, smile because it happened.",
+					"A break up is like a broken mirror. It is better to leave it broken than to hurt yourself trying to put it back together.",
+					"The hardest thing in life is to watch the one you love, love someone else..",
+					"Love starts with a hug, grows with a kiss, and ends with a tear.",
+					"The hardest part is waking up in the morning and remembering what you had been trying so hard to forget last night.",
+					"A heart broken is a lesson learned.",
+					"No matter how much you care, some people don't deserve you.",
+					"Sometimes we create our own heartbreaks through expectation.",
+					"Never let an old flame burn you twice."];
+			var randNum= Math.floor(Math.random()*10);
+			document.getElementById('txtEslq').innerHTML = SLQ[randNum];
+			}
+			
+	
+	function randomEnglishLoveQuotes()
+			{
+			var ELQ=["Love is not finding someone to live with, It's finding someone you can't live without.",
+					"Love can make you do things that you never thought possible",
+					"Do you ever put your arms out and spin and spin and spin? Well that's what love is like. Everything around you tells you to stop before you fall, but you just keep going.",
+					"Happiness is to love and to be loved.",
+					"Love is like heaven but hurts like hell.",
+					"Sometimes your mind doesn't want you to be in love, but deep down, you know you are.",
+					"True love doesn't have a happy ending, because true love never ends.",
+					"You call it Madness, but I call it Love",
+					"It is never too late to fall in love.",
+					"Love is like an hour glass, with the heart filling up as the brain empties."];
+			var randNum= Math.floor(Math.random()*10);
+			document.getElementById('txtElq').innerHTML = ELQ[randNum];
+			}
+			
+	function randomTagalogSadLoveQuotes()
+			{
+			var TSLQ=["Minsan kung sino pa yng rason mo kung bakit ka masaya, Siya din ang rason kung bakit ka nasasaktan ng sobra.",
+					"Sana SINUSULAT nalang yung NARARAMDAMAN para Madaling BURAHIN pag nasaktan!",
+					"Masakit kapag yung taong mahal mo mas masaya sa piling ng iba, pero diba mas masakit isipin kung masaya ka para sa kanila pero sa isip mo, sana ako na lang sya.",
+					"Sabi nila kapag mahal mo, lahat gagawin mo, lahat ibibigay mo, pero paano na kung hilingin nyang palayain mo na sya? mabibigay mo pa? kahit mahal mo pa sya.",
+					"Ang pagmu-move on, parang laro yan. Kung sino ang unang naging masaya, siya yung panalo. Ang tanong, magpapatalo ka ba?",
+					"Sana kung gaano kadali ang ma-in love, ganun din kadali ang mag-move on at makalimot.",
+					"Hindi naman sa napagod ka nang magpatawad. Siguro, napagod ka lang na masaktan ulit.",
+					"Ang hirap bitawan nung taong kahit hindi kayo, siya yung nagpapasaya at kumukumpleto ng araw mo!",
+					"Ang sakit ng feeling kapag ni-reject ka niya. Pero mas pinaka masakit ang feeling kapag paulit-ulit ka ng ni-reject pero umaasa ka parin.",
+					"Nagseselos ako sa mga taong nagiging malapit sayo lalo na pag wala ako.",
+					"Akala ko ikaw na talaga , nag joke lang pala yung tadhana.",
+					"Malungkot ka kasi wala na sya? dapat nga matuwa ka pa kasi masaya na sya."];
+			var randNum= Math.floor(Math.random()*12);
+			document.getElementById('txtTslq').innerHTML = TSLQ[randNum];
+			}
+			
+			
+			
